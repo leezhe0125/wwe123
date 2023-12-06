6ba98f72065856382c283049def6a27ae4ab6e95
leezhe0125 <128338629+leezhe0125@users.noreply.github.com>
2023-12-06 14:39:33 +0800

    Update index.py

--- index.py
+++ index.py
@@ -63,7 +63,27 @@ def search():
         return Result
     else:
         return render_template("search.html")
-    
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    import requests,json
+url = "https://datacenter.taichung.gov.tw/swagger/OpenData/db36e286-1d2b-4784-99b9-3b0790dd9652"
+Data = requests.get(url)
+#print(Data.text)
+
+JsonData = json.loads(Data.text)
+
+Road = input("請輸入慾查詢的路段關鍵字:")
+
+for x in JsonData:
+	if Road in x["路口名稱"]:
+		print(x["路口名稱"] + "總共發生了" + x["總件數"] + "次車禍")
+		print("事故發生的主要原因是因爲" + x["主要肇因"])
+		print()
+        return Result
+    else:
+        return render_template("search.html")
+        
 @app.route("/read")
 def read():
     Result = ""
